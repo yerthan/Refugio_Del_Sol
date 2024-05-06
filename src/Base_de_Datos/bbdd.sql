bb153c1fe7f932161ef715da2dec393c790377ef
Carmen <cmardom599@g.educaand.es>
2024-05-06 12:36:01 +0200

    bbdd precios

--- src/Base_de_Datos/bbdd.sql
+++ src/Base_de_Datos/bbdd.sql
@@ -3,7 +3,7 @@ CREATE database IF NOT EXISTS refugio_del_sol;
 USE refugio_del_sol;
 
 CREATE TABLE Habitacion (
-                            ID INT PRIMARY KEY,
+                            ID INT PRIMARY KEY AUTO_INCREMENT,
                             Numero INT,
                             Tipo VARCHAR(50),
                             Capacidad INT,
@@ -11,20 +11,20 @@ CREATE TABLE Habitacion (
 );
 
 CREATE TABLE Extras (
-                        ID INT PRIMARY KEY,
+                        ID INT PRIMARY KEY AUTO_INCREMENT,
                         Nombre VARCHAR(50),
                         Precio DECIMAL(5,2)
 );
 
 CREATE TABLE Regimen (
-                         ID INT PRIMARY KEY,
+                         ID INT PRIMARY KEY AUTO_INCREMENT,
                          Tipo VARCHAR(50),
                          Descripcion VARCHAR(255),
                          Precio DECIMAL(5,2)
 );
 
 CREATE TABLE Reserva (
-                         ID INT PRIMARY KEY,
+                         ID INT PRIMARY KEY AUTO_INCREMENT,
                          FechaInicio DATE,
                          FechaFin DATE,
                          HabitacionID INT,
@@ -34,3 +34,33 @@ CREATE TABLE Reserva (
                          FOREIGN KEY (ExtrasID) REFERENCES Extras(ID),
                          FOREIGN KEY (RegimenID) REFERENCES Regimen(ID)
 );
+
+
+-- Habitaciones con capacidad 1
+INSERT INTO Habitacion (Numero, Tipo, Capacidad, Precio) VALUES
+                                                             (1, 'suite', 1, 0),
+                                                             (2, 'suite', 1, 0),
+                                                             (3, 'suite', 1, 0),
+                                                             (4, 'turista', 1, 100),
+                                                             (5, 'turista', 1, 100),
+                                                             (6, 'turista', 1, 100);
+
+-- Habitaciones con capacidad 2
+INSERT INTO Habitacion (Numero, Tipo, Capacidad, Precio) VALUES
+                                                             (7, 'suite', 2, 50),
+                                                             (8, 'suite', 2, 50),
+                                                             (9, 'suite', 2, 50),
+                                                             (10, 'turista', 2, 150),
+                                                             (11, 'turista', 2, 150),
+                                                             (12, 'turista', 2, 150),
+                                                             (13, 'turista', 2, 150);
+
+-- Habitaciones con capacidad 3
+INSERT INTO Habitacion (Numero, Tipo, Capacidad, Precio) VALUES
+                                                             (14, 'suite', 3, 80),
+                                                             (15, 'suite', 3, 80),
+                                                             (16, 'suite', 3, 80),
+                                                             (17, 'turista', 3, 180),
+                                                             (18, 'turista', 3, 180),
+                                                             (19, 'turista', 3, 180),
+                                                             (20, 'suite', 3, 280);
