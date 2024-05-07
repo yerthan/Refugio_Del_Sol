2ad3cf2fc03f991d77316b8fc0677a14cc9503f3
Carmen <cmardom599@g.educaand.es>
2024-05-07 12:41:50 +0200

    carpetas

--- src/controlador/claseControlador.java
+++ src/controlador/claseControlador.java
@@ -1,4 +1,34 @@
 package controlador;
 
-public class claseControlador {
-}
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+//
+//public class claseControlador {
+//    public String escribirInfo(String informacion){
+//        String resultado = "Reserva realizada";
+//        try {
+//            BufferedWriter bw=new BufferedWriter(new FileWriter("infoReserva.txt"));
+//            if(informacion != null){
+//                informacion = informacion.substring(0, informacion.length() - 1);
+//
+//            }
+//
+//        }catch (IOException e){
+//            System.out.println("error");
+//            System.out.println(e);
+//        }
+//
+//
+//        return resultado;
+//    }
+//
+//
+//    public String enviarInformacion(String nombreFichero) {
+//        String resultado="Pedido almacenado correctamente";
+//        PedidoDao pdDao=new PedidoDao();
+//        resultado = pdDao.procesarPedido(nombreFichero);
+//        return resultado;
+//
+//    }
+//}
--- src/operaciones/LecturaOEscrituraFicheros.java
+++ src/operaciones/LecturaOEscrituraFicheros.java
@@ -7,30 +7,5 @@ import java.io.IOException;
 
 public class LecturaOEscrituraFicheros {
 
-        public String escribirInfo(String informacion){
-            String resultado = "Reserva realizada";
-            try {
-                BufferedWriter bw=new BufferedWriter(new FileWriter("infoReserva.txt"));
-                if(informacion != null){
-                    informacion = informacion.substring(0, informacion.length() - 1);
 
-                }
-
-            }catch (IOException e){
-                System.out.println("error");
-                System.out.println(e);
-            }
-
-
-            return resultado;
-        }
-
-
-    public String enviarInformacion(String nombreFichero) {
-        String resultado="Pedido almacenado correctamente";
-        PedidoDao pdDao=new PedidoDao();
-        resultado = pdDao.procesarPedido(nombreFichero);
-        return resultado;
-
-    }
 }
