30b6e12b7ea9c01c1d11ab7e0de9811c6ad32541
Carmen <cmardom599@g.educaand.es>
2024-05-07 12:52:47 +0200

    clase extras, regimen, unique en id

---
+++ src/operaciones/Extras.java
@@ -0,0 +1,49 @@
+package operaciones;
+
+public class Extras {
+    private int ID;
+    private String nombre;
+    private double precio;
+
+    public Extras(int ID, String nombre, double precio) {
+        this.ID = ID;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Extras() {
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Extras{" +
+                "ID=" + ID +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
---
+++ src/operaciones/Regimen.java
@@ -0,0 +1,50 @@
+package operaciones;
+
+public class Regimen {
+    private int ID;
+    private String tipo;
+    private String descripcion;
+    private double precio;
+
+    public Regimen(int ID, String tipo, String descripcion, double precio) {
+        this.ID = ID;
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public Regimen() {
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
