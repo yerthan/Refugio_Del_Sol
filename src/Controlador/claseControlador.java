3c4584982eb76b532e9a2fc6d896ec2db5b05abb
Carmen <cmardom599@g.educaand.es>
2024-05-14 09:54:08 +0200

    comentarios quitdos

--- src/Controlador/claseControlador.java
+++ src/Controlador/claseControlador.java
@@ -9,33 +9,6 @@ import java.util.ArrayList;
 
 //
 public class claseControlador {
-    /*public String escribirInfo(ArrayList<String> arrayList){
-        String resultado = "Reserva realizada";
-        try {
-            BufferedWriter bw=new BufferedWriter(new FileWriter("./src/ArchivoDeTextos/infoReserva.txt"));
-            if(arrayList != null){
-                arrayList = arrayList.substring(0, informacion.length() - 1);
-
-            }
-
-        }catch (Exception e){
-            System.out.println("error");
-            System.out.println(e);
-        }
-
-
-        return resultado;
-    }
-
-
-    public String enviarInformacion(String nombreFichero) {
-        String resultado="Pedido almacenado correctamente";
-        PedidoDao pdDao=new PedidoDao();
-        resultado = pdDao.procesarPedido(nombreFichero);
-        return resultado;
-
-    }
-    */
 
     public static ArrayList<String> leerReserva(){
 
@@ -61,7 +34,6 @@ public class claseControlador {
             System.out.println(e);
 
         }
-            //hola
 
         return null;
 
